9bccdf36a2a30f05827c7c9265115bf87cd3009e
Valdemir Souza <souza.valdemir@aluno.ifsp.edu.br>
2023-08-17 17:44:53 -0300

    aprendizado react inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Header from "./Header"
+import Sidebar from "./Sidebar"
+import Content from "./Content"
+import Footer from "./Footer"
+
+import "./App.css"
 
 const App = () => {
   return (
     <>
        <Header />
+       <div id="main">
+          <Sidebar />
+          <Content />
+       </div>
+       <Footer />
     </>
   )
 }
 
 export default App
-
