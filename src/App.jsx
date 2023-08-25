f32b8c91fb3bacac8ba8688c95e37a988a525263
Valdemir <souza.valdemir@aluno.ifsp.edu.br>
2023-08-25 12:27:48 -0300

    Atualização do meu Note para PC-If

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Home from "./components/pages/Home"
-
+import QuemSomos from "./components/pages/QuemSomos"
+import Page404 from "./components/pages/Page404"
+import {Routes, Route} from "react-router-dom"
 import "./App.css"
-import { Routes } from "react-router-dom"
-
 
 const App = () => {
   return (
     <Routes>
-      <Route> path="/" element={<Home/>} />
-      <Route> path="/quem somos" element={<QuemSomos/>} />
-      <Route> path="element"={<Contato/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/quem-somos" element={<QuemSomos />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
-     )
+  )
 }
 
 export default App
--- src/components/pages/QuemSomos.jsx
+++ src/components/pages/QuemSomos.jsx
@@ -1,20 +1,19 @@
 import Header from "../layout/Header"
+import Sidebar from "../layout/Sidebar"
+import Content from "../layout/Content"
 import Footer from "../layout/Footer"
 
-
-const Page404 = () => {
+const QuemSomos = () => {
   return (
-     <>
-         <Header />
-         <h1>Quem Somos Nós:
-            
-         What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-         </h1>
-
-     </>
-
+    <>
+        <Header />
+        <div id="main">
+            <Sidebar />
+            <h1>Quem Somos</h1>
+            <Content />
+        </div>
+        <Footer />
+    </>
   )
 }
 
