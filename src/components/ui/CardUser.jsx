88bf0abcc5ad0fd712ddd635cb29e035aa8d8e7a
Valdemir Souza <souza.valdemir@aluno.ifsp.edu.br>
2023-10-02 17:50:37 -0300

    atualizações react-Quem Somos

--- src/components/ui/CardUser.jsx
+++ src/components/ui/CardUser.jsx
@@ -1,15 +1,14 @@
 import './CardUser.css'
 
-const CardUser = () => {
+const CardUser = ({user}) => {
   return (
-        <div className="card-user">
-            <img src="https://avatars.githubusercontent.com/u/4259630?v=4" alt="Prof.Renan" />
-       <div>
-              <h3>Prof Renan</h3>
-              <span>profrenan@hotmail.com</span>
-       </div>
-       
+    <div className="card-user">
+        <img src={user.photo} alt={user.name} />
+        <div>
+            <h3>{user.name}</h3>
+            <span>{user.email}</span>
         </div>
+    </div>
   )
 }
 
